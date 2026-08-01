4b7396ccc6018dedd1187f3290f739f1c9fa003d
bhadanepiyusha <bhadanepiyusha04@gmail.com>
2026-08-01 20:43:48 +0530

    Add files via upload

---
+++ Binary_Search.cpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
