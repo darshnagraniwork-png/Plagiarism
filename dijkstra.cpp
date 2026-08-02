a44964d7833b03aaa75b1474f66d32f8beaac471
Tejas Halvankar <tejashalvankar2006@gmail.com>
2026-08-03 01:25:28 +0530

    Implement Dijkstra's algorithm for shortest paths

---
+++ dijkstra.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+const int INF = 1e9;
+
+void dijkstra(int start, int n, const vector<vector<pair<int, int>>>& adj, vector<int>& dist) {
+    dist.assign(n, INF);
+    dist[start] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        int d = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if (d > dist[u]) continue;
+
+        for (auto edge : adj[u]) {
+            int v = edge.first;
+            int weight = edge.second;
+
+            if (dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
